90661511a2b9c3d76a5bc22a04fbd59e13e85a0d
denisbarca <dbarca701@gmail.com>
2025-01-31 15:38:09 +0100

    deploy universally

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/digital_twin_temp_ciampino_unigis/",
+    publicPath: "./",
     clean: true
   },
   module: {
